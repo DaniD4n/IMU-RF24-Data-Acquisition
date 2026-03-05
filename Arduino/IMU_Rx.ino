402fcad3052b5e4a0e66c51dd160de5e8ca2db51
P. Dani <137707302+DaniD4n@users.noreply.github.com>
2026-03-05 13:14:12 -0300

    Add files via upload

---
+++ Arduino/IMU_Rx.ino
@@ -0,0 +1,43 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <RF24.h>
+
+// NRF SETUP
+RF24 radio(9, 10); // CE, CSN
+const byte address[6] = "00001";
+
+// recriando a struct
+struct __attribute__((packed)) IMUData {
+  uint32_t timestamp;
+  int16_t ax, ay, az;
+  int16_t gx, gy, gz;
+};
+
+void setup(){
+  Serial.begin(115200);
+  Serial.println("recebendo");
+
+  radio.begin();
+  radio.setPALevel(RF24_PA_LOW);
+  //radio.setDataRate(RF24_1MBPS);
+  radio.openReadingPipe(0, address);
+  radio.startListening();
+
+};
+
+void loop(){
+  if(radio.available()){
+    IMUData data;
+    radio.read(&data, sizeof(data));
+
+     // Header
+    Serial.write(0xAA);
+    Serial.write(0x55);
+
+    // Envia a struct inteira em binário
+    Serial.write((uint8_t*)&data, sizeof(data));
+  
+
+  };
+
+}
